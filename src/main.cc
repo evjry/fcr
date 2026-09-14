c7c58e689e0e62f9ad57f2047f021667f9e4624e
evjry <evjry@icloud.com>
2026-09-14 17:35:51 +1000

    Separated -n and -b options into separate files and cleaned up code

--- src/main.cc
+++ src/main.cc
@@ -4,31 +4,51 @@
 #include <string>
 #include <vector>
 
+#include "include/num_lines.h"
+#include "include/blank_lines.h"
+
 int main(int argc, char **argv){
+    bool num_lines = false;
+    bool blank_lines = false;
+
     // checking if command has any argument.
     if (argc < 2){
+        std::cout << "fcr: " << std::endl;
+        std::cout << "-n [Number all lines]" << std::endl;
+        std::cout << "-b [Number all non-blank lines]" << std::endl;
         return 0;
     }
 
     // going through each command argument.
     // starting with 1, because 0 argument is the command name.
-    for (int i = 1; i < argc; ++i){
-        int rf = open(argv[i], O_RDONLY);
+    for (int arg = 1; arg < argc; ++arg){
+        char buffer[4096];
+        ssize_t bytes_read;
 
-        if (rf == -1){
-            std::cerr << "Error: fcr couldnt open " << argv[i] << '\n';
+        if (std::string(argv[arg]) == "-n"){
+            num_lines = true;
+            continue;
+        }
+        if (std::string(argv[arg]) == "-b"){
+            blank_lines = true;
             continue;
         }
 
-        char buffer[4096];
-        ssize_t bytes_read;
+        int rf = open(argv[arg], O_RDONLY);
+
+        if (rf == -1){
+            std::cerr << "Error: fcr couldnt open " << argv[arg] << '\n';
+            continue;
+        }
+
+        if (num_lines){num_print(rf);}
+        if (blank_lines){blank_print(rf);}
         
-        //while read up to 4096 bytes at a time from the file(s)
-        //and keep reading until there's nothing left.
         while ((bytes_read = read(rf, buffer, sizeof(buffer))) > 0){
-            //write standartOUT_FILENO, buffer(get characters out of buffer, bytes_read)
+            //output everything that was in bytes_read
             write(STDOUT_FILENO, buffer, bytes_read);
         }
+
         std::cout << std::endl;
         close(rf);
     }
