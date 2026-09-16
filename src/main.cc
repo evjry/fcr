62339776951e865862cdcde9c79e45f3b9aaba17
evjry <evjry@icloud.com>
2026-09-16 17:19:36 +1000

    Added pipe support, allowed stdin without any arguments and improved file open errors.

--- src/main.cc
+++ src/main.cc
@@ -3,28 +3,46 @@
 #include <fcntl.h>
 #include <string>
 #include <vector>
+#include <errno.h>
 
 #include "include/num_lines.h"
 #include "include/blank_lines.h"
+#include "include/show_ends.h"
 
 int main(int argc, char **argv){
+    int arg = 1;
+    char buffer[4096];
+    ssize_t bytes_read;
+
     bool num_lines = false;
     bool blank_lines = false;
+    bool show_ends = false;
+    bool rstring = false;
 
-    // checking if command has any argument.
-    if (argc < 2){
-        std::cout << "fcr: " << std::endl;
-        std::cout << "-n [Number all lines]" << std::endl;
-        std::cout << "-b [Number all non-blank lines]" << std::endl;
-        return 0;
+    //if (argc < 2){
+    //    std::cout << "fcr: " << std::endl;
+    //    std::cout << "-n [Number all lines]" << std::endl;
+    //    std::cout << "-b [Number all non-blank lines]" << std::endl;
+    //    std::cout << "-e [Show end of each line by printing $ sign]" << std::endl;
+    //    return 0;
+    //}
+    
+    if (argc == 1){
+        while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0){
+            write(STDOUT_FILENO, buffer, bytes_read);
+            return 0;
+        }
+    }
+    if (std::string(argv[arg]) == "-"){
+        while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0){
+            write(STDOUT_FILENO, buffer, bytes_read);
+            return 0;
+        }   
     }
 
     // going through each command argument.
     // starting with 1, because 0 argument is the command name.
     for (int arg = 1; arg < argc; ++arg){
-        char buffer[4096];
-        ssize_t bytes_read;
-
         if (std::string(argv[arg]) == "-n"){
             num_lines = true;
             continue;
@@ -33,19 +51,23 @@ int main(int argc, char **argv){
             blank_lines = true;
             continue;
         }
+        if (std::string(argv[arg]) == "-e"){
+            show_ends = true;
+            continue;
+        }
 
         int rf = open(argv[arg], O_RDONLY);
 
         if (rf == -1){
-            std::cerr << "Error: fcr couldnt open " << argv[arg] << '\n';
+            perror("fcr");
             continue;
         }
 
         if (num_lines){num_print(rf);}
         if (blank_lines){blank_print(rf);}
-        
+        if (show_ends){end_print(rf);}
+
         while ((bytes_read = read(rf, buffer, sizeof(buffer))) > 0){
-            //output everything that was in bytes_read
             write(STDOUT_FILENO, buffer, bytes_read);
         }
 
